567ebea861cb831d8077e27815557fb52b23b912
Peter O'Donoghue <1848579+podonoghue@users.noreply.github.com>
2021-05-16 16:17:30 +1000

    V5.2.0 Firmware - Fixes to USB (stability) Fixes to HCS08 memory writes - was badly broken

--- Firmware/bdm_kinetis_mk22f/Sources/configure.h
+++ Firmware/bdm_kinetis_mk22f/Sources/configure.h
@@ -113,8 +113,8 @@
 //! Software Version Information
 //
 #define VERSION_MAJOR 5
-#define VERSION_MINOR 1
-#define VERSION_MICRO 1
+#define VERSION_MINOR 2
+#define VERSION_MICRO 0
 #define VERSION_SW  ((VERSION_MAJOR<<4)+VERSION_MINOR)
 
 #define VENDOR_ID        (0x16D0)       // Vendor (actually MCS)
